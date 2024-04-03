ff4854ec5bead9eef4d4ae15c013c2a9f890e30f
jdeifel <jdeifel@hawk.iit.edu>
2024-04-02 21:10:24 -0500

    Add output changer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function init(){
   var button = document.getElementById('entrybutton');
-  var input = document.getElementById('entryinput')
+  var input = document.getElementById('entryinput');
+  var output = document.getElementById('textoutput');
     
   entrybutton.addEventListener('click', function() {
     alert('John Deifel: ' + input.value);
+    output.innerHTML = input.value;
   })
   
   }
